53959a4b4daf5de37fbfc47d95b82199a14ab274
Jezebel1990 <jezebelguedes@gmail.com>
2023-07-21 18:02:08 -0300

    create reducer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import './App.css';
 
 function App() {
   return (
+    <Box 
+      component="form"
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      '& > :': { 
+         m: 1,
+         width: '45ch',
+         
+    },
+    textAlign: 'center',
+  }}
+    noValidate
+    autoComplete="off"
+    >
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Minhas Receitas</h2>
+      <TextField id="outlined-basic"  variant="outlined" />
+      <Button 
+      variant="contained"
+       color="primary"
+       sx={{
+        width:"80px",
+        marginLeft:'10px',
+        height: '55px',
+      }}
+       >
+       Buscar
+      </Button>
     </div>
+    </Box>
   );
 }
 
---
+++ src/redux/api.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const YOUR_APP_KEY = "2c3c813a5b4d8fc91cb6833a6374fa88";
+const YOUR_APP_ID = "eaae0a2c";
+
+
+
+export const getRecipes = async (query) => {
+    const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
+    await axios.get(url)
+}
+
